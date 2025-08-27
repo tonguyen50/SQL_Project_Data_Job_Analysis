313ccd790f7b844f88985a9484d95fe34cc7ce93
Luan Nguyen <luan.ng.nguyen@gmail.com>
2025-08-27 15:02:34 -0700

    upload sql files

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,27 @@
+-- What are the skills required for the top paying data analyst jobs?
+
+WITH top_paying_jobs AS (
+    SELECT
+        job_id,
+        job_title,
+        salary_year_avg,
+        name AS company_name
+    FROM 
+        job_postings_fact jobs
+    LEFT JOIN company_dim company ON jobs.company_id = company.company_id
+    WHERE
+        job_title_short = 'Data Analyst' AND
+        job_location = 'Anywhere'AND 
+        salary_year_avg IS NOT NULL
+    ORDER BY salary_year_avg DESC
+    LIMIT 10
+)
+
+SELECT 
+    top_paying_jobs.*,
+    skills
+FROM top_paying_jobs
+INNER JOIN skills_job_dim ON top_paying_jobs.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+ORDER BY
+    salary_year_avg DESC
